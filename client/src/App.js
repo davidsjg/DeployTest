aebd317aafa3727133aa2317b40b0bc17920e0dc
Joey Davidson <j.davidson18@gmail.com>
2021-10-08 16:17:46 -0600

    react redux store created and wrapped around App

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,22 @@ import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
 import memories from "./images/memories.png";
 
+import useStyles from "./styles";
+
 const App = () => {
+  const classes = useStyles();
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <img src={memories} alt="memories" height="60" />
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
       </AppBar>
       <Grow in>
         <Container>
