a3eeacd63b4a2c81a483c48f58589fb82654c164
Joey Davidson <j.davidson18@gmail.com>
2021-10-08 14:06:41 -0600

    FE working, backend next

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,9 @@
-import { useState, useEffect } from "react";
-import "./App.css";
+import React from "react";
 
-// Components
-import Card from "./components/Card";
-import Pagination from "./components/Pagination";
-
-const App = ({ match }) => {
-  const pageNumber = match.params.pageNumber || 1;
-
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const [page, setPage] = useState(pageNumber);
-  const [pages, setPages] = useState(1);
-
-  useEffect(() => {
-    const fecthPosts = async () => {
-      setLoading(true);
-      try {
-        const res = await fetch(`/api/v1/posts?page=${page}`);
-
-        const { data, pages: totalPages } = await res.json();
-
-        setPages(totalPages);
-        setPosts(data);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-        setLoading(false);
-        setError("Some error occured");
-      }
-    };
-
-    fecthPosts();
-  }, [page]);
-
+const App = () => {
   return (
-    <div className="app">
-      {loading ? (
-        <h3 className="loading-text">Loading...</h3>
-      ) : error ? (
-        <h3 className="error-text">{error}</h3>
-      ) : (
-        <>
-          <Pagination page={page} pages={pages} changePage={setPage} />
-          <div className="app__posts">
-            {posts.map((post) => (
-              <Card key={post._id} post={post} />
-            ))}
-          </div>
-          <Pagination page={page} pages={pages} changePage={setPage} />
-        </>
-      )}
+    <div>
+      <h1>App</h1>
     </div>
   );
 };
